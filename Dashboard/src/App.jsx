951f9058165df18beefd331130859e2eb7559141
xtc-software <dillon6654@gmail.com>
2023-07-14 21:10:36 -0700

    .

--- Dashboard/src/App.jsx
+++ Dashboard/src/App.jsx
@@ -7,6 +7,12 @@ function App() {
     
     return (
     <>
+        <head>
+            <meta charset="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Dashboard | Purchy</title>
+        </head>
         <Navbar />
         <div className="dashboard-container">
             <div className="notifications"><BiBell /></div>
