0eeb444a90921ab5fef8c93ab7d889895a296eae
xtc-software <dillon6654@gmail.com>
2023-07-27 22:51:09 -0700

    Front-end changes

--- Pre-release/src/App.jsx
+++ Pre-release/src/App.jsx
@@ -1,23 +1,30 @@
 import { useState } from "react"
 import Modal from "./components/Modal"
 
+
 export default function() {
     const [openModal, setOpenModal] = useState(false)
+
     return (
         <>
             <div className="newsletter-container">
                 <div className="newsletter-frame">
-                <div className="left">
-                    <label htmlFor="input">
-                    <h1>Want to know when <br /> Purchy releases?</h1>
-                    <h3>Be the first to find out by joining our newsletter!</h3></label>
+                    <div className="left">
+                        <label htmlFor="input">
+                        <h1>Want to know when <br /> Purchy releases?</h1>
+                        <h3>Be the first to find out by joining our newsletter!</h3></label>
+                    </div>
+                    <div className="vl"></div>
+                    <div className="right">
+                        <button type="button" onClick={ (() => setOpenModal(true)) }>Subscribe with Discord!</button>
+                    </div>
                 </div>
-                <div className="vl"></div>
-                <div className="right">
-                    <button type="button" onClick={() => setOpenModal(true)}>Subscribe with Discord!</button>
-                </div>
-                </div>
-                <Modal header="Are you sure?" description="By clicking accept you agree to be added to our mailing list using the email attached to your Discord account." open={ openModal } onClose={ () => setOpenModal(false) } />
+                <Modal 
+                    header="Are you sure?" 
+                    description="By clicking accept you agree to be added to our mailing list using the email attached to your Discord account." 
+                    open={ openModal } 
+                    onClose={ () => { setOpenModal(false) }}
+                />
             </div>
         </>
     )
--- Pre-release/src/components/Modal.jsx
+++ Pre-release/src/components/Modal.jsx
@@ -4,12 +4,16 @@ export default function Modal({ header, description, open, agree, onClose }) {
     if (!open) return null;
     return(
         <>
-        <div className="overlay" onClick={onClose}>
+        <div className="overlay" onClick={ onClose } onKeyUp={ onClose }>
             <div className="modal" onClick={ (e) => { e.stopPropagation(); } }>
                 <div className="title">{header}</div>
                 <div className="description">{description}</div>
-                <a href="https://discord.com/api/oauth2/authorize?client_id=1119456207128768665&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fdiscord&response_type=code&scope=identify%20email"><button className="agree-btn">Agree</button></a>
-                <button className="close-modal" onClick={ onClose }>Decline</button>
+                <div className="buttons">
+                    <a href="https://discord.com/api/oauth2/authorize?client_id=1119456207128768665&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fdiscord&response_type=code&scope=identify%20email">
+                        <button className="agree-btn">Agree</button>
+                    </a>
+                    <button className="close-modal" onClick={ onClose }>Decline</button>
+                </div>
             </div>
         </div>
         </>
